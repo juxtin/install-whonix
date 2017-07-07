40b7948ae98904fe28dc6806a454cb40ccf819d4
Justin Holguin <justin.h.holguin@gmail.com>
2017-07-06 21:57:50 -0700

    install_deps: use sudo in script

--- install_deps.sh
+++ install_deps.sh
@@ -4,8 +4,9 @@ set -euo pipefail
 source ./common.sh
 
 step "Installing required dependencies."
-apt-get update
-apt-get install -y qemu-kvm libvirt-bin virt-manager libvirt-daemon-system libvirt-clients
+substep "Requesting sudo password to install packages."
+sudo apt-get update
+sudo apt-get install -y qemu-kvm libvirt-bin virt-manager libvirt-daemon-system libvirt-clients
 
 step "Done! You may also want to install and enable apparmor if you haven't already."
 step "The next step is 'user_setup.sh'."
